bf334215cb87fd064e67869c516e98c9dd7b7d84
k1LoW <k1lowxb@gmail.com>
2018-05-28 22:58:09 +0900

    `tbls dot` generate dot to STDOUT

--- output/dot/dot.go
+++ output/dot/dot.go
@@ -1,50 +1,27 @@
 package dot
 
 import (
-	"fmt"
 	"github.com/k1LoW/tbls/schema"
+	"io"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"text/template"
 )
 
-// Output generate dot file for full relation.
-func Output(s *schema.Schema, path string, force bool) error {
-	fullPath, err := filepath.Abs(path)
-	if err != nil {
-		return err
-	}
-
-	if !force && outputExists(s, fullPath) {
-		return fmt.Errorf("Error: %s", "output file already exists.")
-	}
-
-	file, err := os.Create(filepath.Join(fullPath, "schema.dot"))
-	defer file.Close()
-	if err != nil {
-		return err
-	}
+// Output generate dot format for full relation.
+func Output(wr io.Writer, s *schema.Schema, table string) error {
 	f, _ := Assets.Open(filepath.Join("/", "schema.dot.tmpl"))
 	bs, _ := ioutil.ReadAll(f)
 	tmpl, err := template.New("index").Parse(string(bs))
 	if err != nil {
 		return err
 	}
-	err = tmpl.Execute(file, map[string]interface{}{
+	err = tmpl.Execute(wr, map[string]interface{}{
 		"Schema": s,
 	})
 	if err != nil {
 		return err
 	}
-	fmt.Printf("%s\n", filepath.Join(path, "schema.dot"))
 
 	return nil
-}
-
-func outputExists(s *schema.Schema, path string) bool {
-	if _, err := os.Lstat(filepath.Join(path, "schema.dot")); err == nil {
-		return true
-	}
-	return false
 }
