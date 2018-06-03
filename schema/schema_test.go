486db83ac85cbceadba871923dbd84029de6d276
k1LoW <k1lowxb@gmail.com>
2018-06-03 14:20:52 +0900

    Add test

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -84,3 +84,73 @@ func TestSchema_Sort(t *testing.T) {
 		t.Errorf("actual %v\nwant %v", actual2, expected2)
 	}
 }
+
+func TestAddAditionalData(t *testing.T) {
+	schema := Schema{
+		Name: "testschema",
+		Tables: []*Table{
+			&Table{
+				Name:    "users",
+				Comment: "users comment",
+				Columns: []*Column{
+					&Column{
+						Name: "id",
+						Type: "serial",
+					},
+					&Column{
+						Name: "username",
+						Type: "text",
+					},
+				},
+			},
+			&Table{
+				Name:    "posts",
+				Comment: "posts comment",
+				Columns: []*Column{
+					&Column{
+						Name: "id",
+						Type: "serial",
+					},
+					&Column{
+						Name: "user_id",
+						Type: "int",
+					},
+					&Column{
+						Name: "title",
+						Type: "text",
+					},
+				},
+			},
+		},
+	}
+	additionalBytes := []byte(`
+---
+relations:
+  -
+    table: posts
+    columns:
+      - user_id
+    parentTable: users
+    parentColumns:
+      - id
+    def: posts->users
+comments:
+  -
+    table: posts
+    columnComments:
+      title: post title
+`)
+	_ = schema.AddAdditionalData(additionalBytes)
+	expected := 1
+	actual := len(schema.Relations)
+	if actual != expected {
+		t.Errorf("actual %v\nwant %v", actual, expected)
+	}
+	posts, _ := schema.FindTableByName("posts")
+	title, _ := posts.FindColumnByName("title")
+	expected2 := "post title"
+	actual2 := title.Comment
+	if actual2 != expected2 {
+		t.Errorf("actual %v\nwant %v", actual2, expected2)
+	}
+}
