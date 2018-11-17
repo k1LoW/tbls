7fb0526ca0399b3755b828ac22d236e987701fdb
k1LoW <k1lowxb@gmail.com>
2018-11-17 23:39:39 +0900

    Add test

--- db/db_test.go
+++ db/db_test.go
@@ -7,38 +7,53 @@ import (
 	"testing"
 )
 
+var tests = []struct {
+	dsn           string
+	tableCount    int
+	relationCount int
+}{
+	{"my://root:mypass@localhost:33306/testdb", 7, 5},
+	{"pg://postgres:pgpass@localhost:55432/testdb?sslmode=disable", 8, 6},
+}
+
 func TestAnalyzeSchema(t *testing.T) {
-	schema, err := Analyze("my://root:mypass@localhost:33306/testdb")
-	if err != nil {
-		t.Errorf("%s", err)
-	}
-	expected := "testdb"
-	actual := schema.Name
-	if actual != expected {
-		t.Errorf("actual %v\nwant %v", actual, expected)
+	for _, tt := range tests {
+		schema, err := Analyze(tt.dsn)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+		expected := "testdb"
+		actual := schema.Name
+		if actual != expected {
+			t.Errorf("actual %v\nwant %v", actual, expected)
+		}
 	}
 }
 
 func TestAnalyzeTables(t *testing.T) {
-	schema, err := Analyze("my://root:mypass@localhost:33306/testdb")
-	if err != nil {
-		t.Errorf("%s", err)
-	}
-	expected := 7
-	actual := len(schema.Tables)
-	if actual != expected {
-		t.Errorf("actual %v\nwant %v", actual, expected)
+	for _, tt := range tests {
+		schema, err := Analyze(tt.dsn)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+		expected := tt.tableCount
+		actual := len(schema.Tables)
+		if actual != expected {
+			t.Errorf("actual %v\nwant %v", actual, expected)
+		}
 	}
 }
 
 func TestAnalyzeRelations(t *testing.T) {
-	schema, err := Analyze("my://root:mypass@localhost:33306/testdb")
-	if err != nil {
-		t.Errorf("%s", err)
-	}
-	expected := 5
-	actual := len(schema.Relations)
-	if actual != expected {
-		t.Errorf("actual %v\nwant %v", actual, expected)
+	for _, tt := range tests {
+		schema, err := Analyze(tt.dsn)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+		expected := tt.relationCount
+		actual := len(schema.Relations)
+		if actual != expected {
+			t.Errorf("actual %v\nwant %v", actual, expected)
+		}
 	}
 }
