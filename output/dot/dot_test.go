de026dea60d585aed42428831852a2e5289b2ec8
k1LoW <k1lowxb@gmail.com>
2018-11-17 19:03:51 +0900

    Add .golden

--- output/dot/dot_test.go
+++ output/dot/dot_test.go
@@ -2,6 +2,9 @@ package dot
 
 import (
 	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/k1LoW/tbls/schema"
@@ -14,31 +17,10 @@ func TestOutputSchema(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	expected := `digraph "testschema" {
-  // Config
-  graph [rankdir=TB, layout=dot, fontname="Arial"];
-  node [shape=record, fontsize=14, margin=0.6, fontname="Arial"];
-  edge [fontsize=10, labelfloat=false, splines=none, fontname="Arial"];
-
-  // Tables
-  "a" [shape=none, label=<<table border="0" cellborder="1" cellspacing="0" cellpadding="6">
-                 <tr><td bgcolor="#EFEFEF"><font face="Arial Bold" point-size="18">a</font> <font color="#666666">[]</font></td></tr>
-                 <tr><td port="a" align="left">a <font color="#666666">[]</font></td></tr>
-                 <tr><td port="a2" align="left">a2 <font color="#666666">[]</font></td></tr>
-              </table>>];
-  "b" [shape=none, label=<<table border="0" cellborder="1" cellspacing="0" cellpadding="6">
-                 <tr><td bgcolor="#EFEFEF"><font face="Arial Bold" point-size="18">b</font> <font color="#666666">[]</font></td></tr>
-                 <tr><td port="b" align="left">b <font color="#666666">[]</font></td></tr>
-                 <tr><td port="b2" align="left">b2 <font color="#666666">[]</font></td></tr>
-              </table>>];
-
-  // Relations
-  "a":a -> "b":b [dir=back, arrowtail=crow,  taillabel=<<table cellpadding="5" border="0" cellborder="0"><tr><td></td></tr></table>>];
-}
-`
+	expected, _ := ioutil.ReadFile(filepath.Join(testdataDir(), "dot_test_schema.dot.golden"))
 	actual := buf.String()
-	if actual != expected {
-		t.Errorf("actual %v\nwant %v", actual, expected)
+	if actual != string(expected) {
+		t.Errorf("actual %v\nwant %v", actual, string(expected))
 	}
 }
 
@@ -48,32 +30,17 @@ func TestOutputTable(t *testing.T) {
 
 	buf := &bytes.Buffer{}
 	_ = OutputTable(buf, ta)
-	expected := `digraph "a" {
-  // Config
-  graph [rankdir=TB, layout=dot, fontname="Arial"];
-  node [shape=record, fontsize=14, margin=0.6, fontname="Arial"];
-  edge [fontsize=10, labelfloat=false, splines=none, fontname="Arial"];
-
-  // Tables
-  "a" [shape=none, label=<<table border="3" cellborder="1" cellspacing="0" cellpadding="6">
-                 <tr><td bgcolor="#EFEFEF"><font face="Arial Bold" point-size="18">a</font> <font color="#666666">[]</font></td></tr>
-                 <tr><td port="a" align="left">a <font color="#666666">[]</font></td></tr>
-                 <tr><td port="a2" align="left">a2 <font color="#666666">[]</font></td></tr>
-              </table>>];
-  "b" [shape=none, label=<<table border="0" cellborder="1" cellspacing="0" cellpadding="6">
-                 <tr><td bgcolor="#EFEFEF"><font face="Arial Bold" point-size="18">b</font> <font color="#666666">[]</font></td></tr>
-                 <tr><td port="b" align="left">b <font color="#666666">[]</font></td></tr>
-                 <tr><td port="b2" align="left">b2 <font color="#666666">[]</font></td></tr>
-              </table>>];
-
-  // Relations
-  "a":a -> "b":b [dir=back, arrowtail=crow,  taillabel=<<table cellpadding="5" border="0" cellborder="0"><tr><td></td></tr></table>>];
-}
-`
+	expected, _ := ioutil.ReadFile(filepath.Join(testdataDir(), "dot_test_a.dot.golden"))
 	actual := buf.String()
-	if actual != expected {
-		t.Errorf("actual %v\nwant %v", actual, expected)
+	if actual != string(expected) {
+		t.Errorf("actual %v\nwant %v", actual, string(expected))
 	}
+}
+
+func testdataDir() string {
+	wd, _ := os.Getwd()
+	dir, _ := filepath.Abs(filepath.Join(filepath.Dir(filepath.Dir(wd)), "testdata"))
+	return dir
 }
 
 func newTestSchema() *schema.Schema {
