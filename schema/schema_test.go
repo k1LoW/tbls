74c246045025c465bd69db96133ebcc282bdb583
k1LoW <k1lowxb@gmail.com>
2018-11-22 10:00:17 +0900

    Add db.AnalizeJSON

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -1,6 +1,8 @@
 package schema
 
 import (
+	"database/sql"
+	"encoding/json"
 	"os"
 	"path/filepath"
 	"testing"
@@ -143,8 +145,105 @@ func TestAddAditionalData(t *testing.T) {
 	}
 }
 
+func TestRepair(t *testing.T) {
+	actual := &Schema{}
+	file, err := os.Open(filepath.Join(testdataDir(), "json_test_schema.json.golden"))
+	if err != nil {
+		t.Error(err)
+	}
+	dec := json.NewDecoder(file)
+	dec.Decode(actual)
+	expected := newTestSchema()
+	err = actual.Repair()
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, tt := range actual.Tables {
+		for j, c := range tt.Columns {
+			for _, pr := range actual.Tables[i].Columns[j].ParentRelations {
+				if pr.Columns[0] != c {
+					t.Errorf("actual %#v\nwant %#v", pr.Columns[0], c)
+				}
+			}
+		}
+	}
+
+	if actual != expected {
+		t.Errorf("actual %v\nwant %v", actual, expected)
+	}
+}
+
 func testdataDir() string {
 	wd, _ := os.Getwd()
 	dir, _ := filepath.Abs(filepath.Join(filepath.Dir(wd), "testdata"))
 	return dir
+}
+
+func newTestSchema() *Schema {
+	ca := &Column{
+		Name:     "a",
+		Type:     "bigint(20)",
+		Comment:  "column a",
+		Nullable: false,
+	}
+	cb := &Column{
+		Name:     "b",
+		Type:     "text",
+		Comment:  "column b",
+		Nullable: true,
+	}
+
+	ta := &Table{
+		Name:    "a",
+		Type:    "BASE TABLE",
+		Comment: "table a",
+		Columns: []*Column{
+			ca,
+			&Column{
+				Name:     "a2",
+				Type:     "datetime",
+				Comment:  "column a2",
+				Nullable: false,
+				Default: sql.NullString{
+					String: "CURRENT_TIMESTAMP",
+					Valid:  true,
+				},
+			},
+		},
+	}
+	tb := &Table{
+		Name:    "b",
+		Type:    "BASE TABLE",
+		Comment: "table b",
+		Columns: []*Column{
+			cb,
+			&Column{
+				Name:     "b2",
+				Comment:  "column b2",
+				Type:     "text",
+				Nullable: true,
+			},
+		},
+	}
+	r := &Relation{
+		Table:         ta,
+		Columns:       []*Column{ca},
+		ParentTable:   tb,
+		ParentColumns: []*Column{cb},
+	}
+	ca.ParentRelations = []*Relation{r}
+	cb.ChildRelations = []*Relation{r}
+
+	s := &Schema{
+		Name: "testschema",
+		Tables: []*Table{
+			ta,
+			tb,
+		},
+		Relations: []*Relation{
+			r,
+		},
+	}
+	return s
 }
