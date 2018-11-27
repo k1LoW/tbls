8a2ed09bb729422b1e6ceb4a81c781452f7e0ab8
k1LoW <k1lowxb@gmail.com>
2018-11-27 09:26:37 +0900

    Add test

---
+++ config/config_test.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"os"
+	"testing"
+)
+
+var tests = []struct {
+	value    string
+	expected string
+}{
+	{"${TBLS_ONE}/${TBLS_TWO}", "one/two"},
+	{"${TBLS_ONE}/${TBLS_TWO}/${TBLS_NONE}", "one/two/"},
+	{"${{TBLS_ONE}}", "${{TBLS_ONE}}"},
+}
+
+func TestParseWithEnvirion(t *testing.T) {
+	_ = os.Setenv("TBLS_ONE", "one")
+	_ = os.Setenv("TBLS_TWO", "two")
+	for _, tt := range tests {
+		actual, err := parseWithEnviron(tt.value)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if actual != tt.expected {
+			t.Errorf("actual %v\nwant %v", actual, tt.expected)
+		}
+	}
+}
