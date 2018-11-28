7f2cd4f5cb1bf90ff426cff5de3325f33992eb89
k1LoW <k1lowxb@gmail.com>
2018-11-29 01:38:04 +0900

    Add test

--- config/config_test.go
+++ config/config_test.go
@@ -2,8 +2,31 @@ package config
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
+
+func TestLoadConfigFile(t *testing.T) {
+	_ = os.Setenv("TBLS_TEST_PG_PASS", "pgpass")
+	_ = os.Setenv("TBLS_TEST_PG_DOC_PATH", "sample/pg")
+	configFilepath := filepath.Join(testdataDir(), "env_testdb_config.yml")
+	config, err := NewConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = config.LoadConfigFile(configFilepath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := "pg://root:pgpass@localhost:55432/testdb?sslmode=disable"
+	if config.DSN != expected {
+		t.Errorf("actual %v\nwant %v", config.DSN, expected)
+	}
+	expected2 := "sample/pg"
+	if config.DocPath != expected2 {
+		t.Errorf("actual %v\nwant %v", config.DocPath, expected2)
+	}
+}
 
 var tests = []struct {
 	value    string
@@ -27,4 +50,10 @@ func TestParseWithEnvirion(t *testing.T) {
 			t.Errorf("actual %v\nwant %v", actual, tt.expected)
 		}
 	}
+}
+
+func testdataDir() string {
+	wd, _ := os.Getwd()
+	dir, _ := filepath.Abs(filepath.Join(filepath.Dir(wd), "testdata"))
+	return dir
 }
