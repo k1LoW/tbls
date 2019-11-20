b5f82e3061a15bb72cc8a95f7039da7e6c7505d4
k1LoW <k1lowxb@gmail.com>
2019-11-20 22:48:01 +0900

    Add test

---
+++ output/config/config_test.go
@@ -0,0 +1,94 @@
+package config
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/k1LoW/tbls/config"
+	"github.com/k1LoW/tbls/schema"
+)
+
+func TestOutputSchema(t *testing.T) {
+	s := newTestSchema()
+	c, err := config.NewConfig()
+	if err != nil {
+		t.Error(err)
+	}
+	o := NewConfig(c)
+	buf := &bytes.Buffer{}
+	err = o.OutputSchema(buf, s)
+	if err != nil {
+		t.Error(err)
+	}
+	expected, err := ioutil.ReadFile(filepath.Join(testdataDir(), "config_test.yml.golden"))
+	if err != nil {
+		t.Error(err)
+	}
+	actual := buf.String()
+	if actual != string(expected) {
+		t.Errorf("actual\n%v\nwant\n%v", actual, string(expected))
+	}
+}
+
+func testdataDir() string {
+	wd, _ := os.Getwd()
+	dir, _ := filepath.Abs(filepath.Join(filepath.Dir(filepath.Dir(wd)), "testdata"))
+	return dir
+}
+
+func newTestSchema() *schema.Schema {
+	ca := &schema.Column{
+		Name:    "a",
+		Comment: "column a",
+	}
+	cb := &schema.Column{
+		Name:    "b",
+		Comment: "column b",
+	}
+
+	ta := &schema.Table{
+		Name:    "a",
+		Comment: "table a",
+		Columns: []*schema.Column{
+			ca,
+			&schema.Column{
+				Name:    "a2",
+				Comment: "column a2",
+			},
+		},
+	}
+	tb := &schema.Table{
+		Name:    "b",
+		Comment: "table b",
+		Columns: []*schema.Column{
+			cb,
+			&schema.Column{
+				Name:    "b2",
+				Comment: "column b2",
+			},
+		},
+	}
+	r := &schema.Relation{
+		Table:         ta,
+		Columns:       []*schema.Column{ca},
+		ParentTable:   tb,
+		ParentColumns: []*schema.Column{cb},
+	}
+	ca.ParentRelations = []*schema.Relation{r}
+	cb.ChildRelations = []*schema.Relation{r}
+
+	s := &schema.Schema{
+		Name: "testschema",
+		Tables: []*schema.Table{
+			ta,
+			tb,
+		},
+		Relations: []*schema.Relation{
+			r,
+		},
+	}
+	return s
+}
