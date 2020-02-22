85382181dd3a9934fd1c18461964a7248ac8e96c
k1LoW <k1lowxb@gmail.com>
2020-02-22 14:57:30 +0900

    Add output format (PNG, SVG, JPG)

---
+++ output/gviz/gviz.go
@@ -0,0 +1,68 @@
+package gviz
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/goccy/go-graphviz"
+	"github.com/k1LoW/tbls/config"
+	"github.com/k1LoW/tbls/output/dot"
+	"github.com/k1LoW/tbls/schema"
+	"github.com/pkg/errors"
+)
+
+// Gviz struct
+type Gviz struct {
+	config *config.Config
+	format string
+	dot    *dot.Dot
+}
+
+// NewGviz return Gviz
+func NewGviz(c *config.Config, format string) *Gviz {
+	return &Gviz{
+		config: c,
+		format: format,
+		dot:    dot.NewDot(c),
+	}
+}
+
+// OutputSchema output dot format for full relation.
+func (g *Gviz) OutputSchema(wr io.Writer, s *schema.Schema) error {
+	buf := &bytes.Buffer{}
+	err := g.dot.OutputSchema(buf, s)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return g.render(wr, buf.Bytes())
+}
+
+// OutputTable output dot format for table.
+func (g *Gviz) OutputTable(wr io.Writer, t *schema.Table) error {
+	buf := &bytes.Buffer{}
+	err := g.dot.OutputTable(buf, t)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return g.render(wr, buf.Bytes())
+}
+
+func (g *Gviz) render(wr io.Writer, b []byte) (e error) {
+	gviz := graphviz.New()
+	graph, err := graphviz.ParseBytes(b)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer func() {
+		if err := gviz.Close(); err != nil {
+			e = errors.WithStack(err)
+		}
+		if err := graph.Close(); err != nil {
+			e = errors.WithStack(err)
+		}
+	}()
+	if err := gviz.Render(graph, graphviz.Format(g.format), wr); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
