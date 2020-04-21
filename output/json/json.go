fbd058baabc8230186571b2d60c13c3a2d0617a1
k1LoW <k1lowxb@gmail.com>
2020-04-21 11:47:33 +0900

    Add Json.inline

--- output/json/json.go
+++ output/json/json.go
@@ -8,12 +8,23 @@ import (
 )
 
 // JSON struct
-type JSON struct{}
+type JSON struct {
+	inline bool
+}
+
+// New returns JSON
+func New(inline bool) *JSON {
+	return &JSON{
+		inline: inline,
+	}
+}
 
 // OutputSchema output JSON format for full relation.
 func (j *JSON) OutputSchema(wr io.Writer, s *schema.Schema) error {
 	encoder := json.NewEncoder(wr)
-	encoder.SetIndent("", "  ")
+	if !j.inline {
+		encoder.SetIndent("", "  ")
+	}
 	err := encoder.Encode(s)
 	if err != nil {
 		return err
@@ -24,7 +35,9 @@ func (j *JSON) OutputSchema(wr io.Writer, s *schema.Schema) error {
 // OutputTable output JSON format for table.
 func (j *JSON) OutputTable(wr io.Writer, t *schema.Table) error {
 	encoder := json.NewEncoder(wr)
-	encoder.SetIndent("", "  ")
+	if !j.inline {
+		encoder.SetIndent("", "  ")
+	}
 	err := encoder.Encode(t)
 	if err != nil {
 		return err
