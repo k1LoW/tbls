54de334d7a8b4e645a18901e1103acfa1fd5bf41
Martijn Pieters <mj@zopatista.com>
2020-04-24 13:15:00 +0100

    Overhaul queries
    - only use pg_catalog tables and functions (faster, cleaner)
    - fetch comments in-line with tables and columns
    - use the oid to get related info (columns, indexes, etc.)
    - use format_type() to produce the correct type name. This means arrays now show up as their contained type with `[]`, and custom domains are reflected correctly.
    
    This is groundwork laid to enable other relationship types (e.g. MATERIALIZED VIEW)

--- drivers/postgres/postgres.go
+++ drivers/postgres/postgres.go
@@ -68,14 +68,22 @@ func (p *Postgres) Analyze(s *schema.Schema) error {
 
 	// tables
 	tableRows, err := p.db.Query(`
-SELECT DISTINCT cls.oid AS oid, cls.relname AS table_name, tbl.table_type AS table_type, tbl.table_schema AS table_schema
-FROM pg_catalog.pg_class cls
+SELECT
+    cls.oid AS oid,
+    cls.relname AS table_name,
+    CASE 
+        WHEN cls.relkind IN ('r', 'p') THEN 'BASE TABLE'
+        WHEN cls.relkind = 'v' THEN 'VIEW'
+        WHEN cls.relkind = 'f' THEN 'FOREIGN TABLE'
+    END AS table_type,
+    ns.nspname AS table_schema,
+    descr.description AS table_comment
+FROM pg_class cls
 INNER JOIN pg_namespace ns ON cls.relnamespace = ns.oid
-INNER JOIN (SELECT table_name, table_type, table_schema
-FROM information_schema.tables
-WHERE table_schema != 'pg_catalog' AND table_schema != 'information_schema'
-AND table_catalog = $1) tbl ON cls.relname = tbl.table_name AND ns.nspname = tbl.table_schema
-ORDER BY oid`, s.Name)
+LEFT JOIN pg_description descr ON cls.oid = descr.objoid AND descr.objsubid = 0
+WHERE ns.nspname NOT IN ('pg_catalog', 'information_schema')
+AND cls.relkind IN ('r', 'p', 'v', 'f')
+ORDER BY oid`)
 	defer tableRows.Close()
 	if err != nil {
 		return errors.WithStack(err)
@@ -86,12 +94,13 @@ ORDER BY oid`, s.Name)
 	tables := []*schema.Table{}
 	for tableRows.Next() {
 		var (
-			tableOid    string
-			tableName   string
-			tableType   string
-			tableSchema string
+			tableOid     uint64
+			tableName    string
+			tableType    string
+			tableSchema  string
+			tableComment sql.NullString
 		)
-		err := tableRows.Scan(&tableOid, &tableName, &tableType, &tableSchema)
+		err := tableRows.Scan(&tableOid, &tableName, &tableType, &tableSchema, &tableComment)
 		if err != nil {
 			return errors.WithStack(err)
 		}
@@ -103,38 +112,12 @@ ORDER BY oid`, s.Name)
 		table := &schema.Table{
 			Name: name,
 			Type: tableType,
-		}
-
-		// table comment
-		tableCommentRows, err := p.db.Query(`
-SELECT pd.description as comment
-FROM pg_stat_user_tables AS ps, pg_description AS pd
-WHERE ps.relid=pd.objoid
-AND pd.objsubid=0
-AND ps.relname = $1
-AND ps.schemaname = $2`, tableName, tableSchema)
-		defer tableCommentRows.Close()
-		if err != nil {
-			return errors.WithStack(err)
-		}
-
-		for tableCommentRows.Next() {
-			var tableComment string
-			err = tableCommentRows.Scan(&tableComment)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-			table.Comment = tableComment
+			Comment: tableComment.String,
 		}
 
 		// view definition
 		if tableType == "VIEW" {
-			viewDefRows, err := p.db.Query(`
-SELECT view_definition FROM information_schema.views
-WHERE table_catalog = $1
-AND table_name = $2
-AND table_schema = $3;
-		`, s.Name, tableName, tableSchema)
+			viewDefRows, err := p.db.Query(`SELECT pg_get_viewdef($1::oid);`, tableOid)
 			defer viewDefRows.Close()
 			if err != nil {
 				return errors.WithStack(err)
@@ -150,7 +133,7 @@ AND table_schema = $3;
 		}
 
 		// constraints
-		constraintRows, err := p.db.Query(p.queryForConstraints(), tableName, tableSchema)
+		constraintRows, err := p.db.Query(p.queryForConstraints(), tableOid)
 		defer constraintRows.Close()
 		if err != nil {
 			return errors.WithStack(err)
@@ -195,14 +178,12 @@ AND table_schema = $3;
 		// triggers
 		if !p.rsMode {
 			triggerRows, err := p.db.Query(`
-SELECT tgname, pg_get_triggerdef(pt.oid)
-FROM pg_trigger AS pt
-LEFT JOIN pg_stat_user_tables AS ps ON ps.relid = pt.tgrelid
-WHERE pt.tgisinternal = false
-AND ps.relname = $1
-AND ps.schemaname = $2
-ORDER BY pt.tgrelid
-`, tableName, tableSchema)
+SELECT tgname, pg_get_triggerdef(oid)
+FROM pg_trigger
+WHERE tgisinternal = false
+AND tgrelid = $1::oid
+ORDER BY tgrelid
+`, tableOid)
 			defer triggerRows.Close()
 			if err != nil {
 				return errors.WithStack(err)
@@ -227,42 +208,28 @@ ORDER BY pt.tgrelid
 			table.Triggers = triggers
 		}
 
-		// columns comments
-		columnCommentRows, err := p.db.Query(`
-SELECT pa.attname AS column_name, pd.description AS comment
-FROM pg_stat_all_tables AS ps ,pg_description AS pd ,pg_attribute AS pa
-WHERE ps.relid=pd.objoid
-AND pd.objsubid != 0
-AND pd.objoid=pa.attrelid
-AND pd.objsubid=pa.attnum
-AND ps.relname = $1
-AND ps.schemaname = $2`, tableName, tableSchema)
-		defer columnCommentRows.Close()
-		if err != nil {
-			return errors.WithStack(err)
-		}
-
-		columnComments := make(map[string]string)
-		for columnCommentRows.Next() {
-			var (
-				columnName    string
-				columnComment string
-			)
-			err = columnCommentRows.Scan(&columnName, &columnComment)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-			columnComments[columnName] = columnComment
-		}
-
 		// columns
 		columnRows, err := p.db.Query(`
-SELECT column_name, column_default, is_nullable, data_type, udt_name, character_maximum_length
-FROM information_schema.columns
-WHERE table_name = $1
-AND table_schema = $2
-ORDER BY ordinal_position
-`, tableName, tableSchema)
+SELECT
+    attr.attname AS column_name,
+    pg_get_expr(def.adbin, def.adrelid) AS column_default,
+    NOT (attr.attnotnull OR tp.typtype = 'd' AND tp.typnotnull) AS is_nullable,
+    CASE
+        WHEN attr.atttypid::regtype = ANY(ARRAY['character varying'::regtype, 'character varying[]'::regtype]) THEN
+            REPLACE(format_type(attr.atttypid, attr.atttypmod), 'character varying', 'varchar')
+        ELSE format_type(attr.atttypid, attr.atttypmod)
+    END AS data_type,
+    descr.description as comment
+FROM pg_attribute attr
+INNER JOIN pg_type tp ON attr.atttypid = tp.oid
+LEFT JOIN pg_attrdef def ON attr.attrelid = def.adrelid AND attr.attnum = def.adnum
+LEFT JOIN pg_description descr ON attr.attrelid = descr.objoid AND attr.attnum = descr.objsubid
+WHERE
+    attr.attnum > 0
+AND NOT attr.attisdropped
+AND attr.attrelid = $1::oid
+ORDER BY attr.attnum;
+`, tableOid)
 		defer columnRows.Close()
 		if err != nil {
 			return errors.WithStack(err)
@@ -273,30 +240,27 @@ ORDER BY ordinal_position
 			var (
 				columnName             string
 				columnDefault          sql.NullString
-				isNullable             string
+				isNullable             bool
 				dataType               string
-				udtName                string
-				characterMaximumLength sql.NullInt64
+				columnComment          sql.NullString
 			)
-			err = columnRows.Scan(&columnName, &columnDefault, &isNullable, &dataType, &udtName, &characterMaximumLength)
+			err = columnRows.Scan(&columnName, &columnDefault, &isNullable, &dataType, &columnComment)
 			if err != nil {
 				return errors.WithStack(err)
 			}
 			column := &schema.Column{
 				Name:     columnName,
-				Type:     convertColmunType(dataType, udtName, characterMaximumLength),
-				Nullable: convertColumnNullable(isNullable),
+				Type:     dataType,
+				Nullable: isNullable,
 				Default:  columnDefault,
-			}
-			if comment, ok := columnComments[columnName]; ok {
-				column.Comment = comment
+				Comment:  columnComment.String,
 			}
 			columns = append(columns, column)
 		}
 		table.Columns = columns
 
 		// indexes
-		indexRows, err := p.db.Query(p.queryForIndexes(), tableName, tableSchema)
+		indexRows, err := p.db.Query(p.queryForIndexes(), tableOid)
 		defer indexRows.Close()
 		if err != nil {
 			return errors.WithStack(err)
@@ -406,72 +370,57 @@ func (p *Postgres) queryForConstraints() string {
 	if p.rsMode {
 		return `
 SELECT
-  pc.conname AS name,
-  pg_get_constraintdef(pc.oid) AS def,
-  contype AS type,
-  NULL,
-  NULL,
-  NULL
-FROM pg_constraint AS pc
-LEFT JOIN pg_stat_user_tables AS ps ON ps.relid = pc.conrelid
-WHERE ps.relname = $1
-AND ps.schemaname = $2
-ORDER BY pc.conrelid, pc.conname`
+  conname, pg_get_constraintdef(oid) AS def, contype, NULL, NULL, NULL
+FROM pg_constraint
+WHERE conrelid = $1::oid
+ORDER BY conname`
 	}
 	return `
 SELECT
-  pc.conname AS name,
-  (CASE WHEN pc.contype='t' THEN pg_get_triggerdef((SELECT oid FROM pg_trigger WHERE tgconstraint = pc.oid LIMIT 1))
-        ELSE pg_get_constraintdef(pc.oid)
-   END) AS def,
-  pc.contype AS type,
-  psf.relname,
-  ARRAY_TO_STRING(ARRAY_AGG(a.attname), ', '),
-  ARRAY_TO_STRING(ARRAY_AGG(af.attname), ', ')
-FROM pg_constraint AS pc
-LEFT JOIN pg_stat_user_tables AS ps ON ps.relid = pc.conrelid
-LEFT JOIN pg_stat_user_tables AS psf ON psf.relid = pc.confrelid
-LEFT JOIN pg_attribute a ON a.attrelid = pc.conrelid
-LEFT JOIN pg_attribute af ON af.attrelid = pc.confrelid
-WHERE ps.relname = $1
-AND ps.schemaname = $2
-AND (a.attnum = ANY(pc.conkey) OR pc.conkey IS NULL)
-AND (af.attnum = ANY(pc.confkey) OR pc.confkey IS NULL)
-GROUP BY pc.conname, pc.contype, pc.oid, psf.relname, pc.conkey, pc.confkey, pc.conrelid, pc.conindid
-ORDER BY pc.conrelid, pc.conindid, pc.conname`
+  cons.conname AS name,
+  CASE WHEN cons.contype = 't' THEN pg_get_triggerdef(trig.oid)
+        ELSE pg_get_constraintdef(cons.oid)
+  END AS def,
+  cons.contype AS type,
+  fcls.relname,
+  ARRAY_TO_STRING(ARRAY_AGG(attr.attname), ', '),
+  ARRAY_TO_STRING(ARRAY_AGG(fattr.attname), ', ')
+FROM pg_constraint AS cons
+LEFT JOIN pg_trigger trig ON trig.tgconstraint = cons.oid AND NOT trig.tgisinternal
+LEFT JOIN pg_class AS fcls ON cons.confrelid = fcls.oid
+LEFT JOIN pg_attribute attr ON attr.attrelid = cons.conrelid
+LEFT JOIN pg_attribute fattr ON fattr.attrelid = cons.confrelid
+WHERE
+	cons.conrelid = $1::oid
+AND (cons.conkey IS NULL OR attr.attnum = ANY(cons.conkey))
+AND (cons.confkey IS NULL OR fattr.attnum = ANY(cons.confkey))
+GROUP BY cons.conindid, cons.conname, cons.contype, cons.oid, trig.oid, fcls.relname
+ORDER BY cons.conindid, cons.conname`
 }
 
 func (p *Postgres) queryForIndexes() string {
 	if p.rsMode {
 		return `
 SELECT
-  i.relname AS indexname,
-  pg_get_indexdef(i.oid) AS indexdef,
+  cls.relname AS indexname,
+  pg_get_indexdef(idx.indexrelid) AS indexdef,
   NULL
-FROM pg_index x
-JOIN pg_class c ON c.oid = x.indrelid
-JOIN pg_class i ON i.oid = x.indexrelid
-LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
-WHERE c.relkind = ANY (ARRAY['r'::"char", 'm'::"char"]) AND i.relkind = 'i'::"char"
-AND c.relname = $1
-AND n.nspname = $2
-ORDER BY x.indexrelid`
+FROM pg_index idx
+INNER JOIN pg_class cls ON idx.indexrelid = cls.oid
+WHERE idx.indrelid = $1::oid
+ORDER BY idx.indexrelid`
 	}
 	return `
 SELECT
-  i.relname AS indexname,
-  pg_get_indexdef(i.oid) AS indexdef,
-  ARRAY_TO_STRING(ARRAY_AGG(a.attname), ', ')
-FROM pg_index x
-JOIN pg_class c ON c.oid = x.indrelid
-JOIN pg_class i ON i.oid = x.indexrelid
-JOIN pg_attribute a ON i.oid = a.attrelid
-JOIN pg_namespace n ON n.oid = c.relnamespace
-WHERE c.relkind = ANY (ARRAY['r', 'm']) AND i.relkind = 'i'
-AND c.relname = $1
-AND n.nspname = $2
-GROUP BY c.relname, n.nspname, i.relname, i.oid, x.indexrelid
-ORDER BY x.indexrelid`
+  cls.relname AS indexname,
+  pg_get_indexdef(idx.indexrelid) AS indexdef,
+  ARRAY_TO_STRING(ARRAY_AGG(attr.attname), ', ')
+FROM pg_index idx
+INNER JOIN pg_class cls ON idx.indexrelid = cls.oid
+INNER JOIN pg_attribute attr on idx.indexrelid = attr.attrelid
+WHERE idx.indrelid = $1::oid
+GROUP BY cls.relname, idx.indexrelid
+ORDER BY idx.indexrelid`
 }
 
 func detectFullTableName(name string, searchPaths, fullTableNames []string) (string, error) {
@@ -495,19 +444,6 @@ func detectFullTableName(name string, searchPaths, fullTableNames []string) (str
 	return fns[0], nil
 }
 
-func convertColmunType(t string, udtName string, characterMaximumLength sql.NullInt64) string {
-	switch t {
-	case "USER-DEFINED":
-		return udtName
-	case "ARRAY":
-		return "array"
-	case "character varying":
-		return fmt.Sprintf("varchar(%d)", characterMaximumLength.Int64)
-	default:
-		return t
-	}
-}
-
 func convertConstraintType(t string) string {
 	switch t {
 	case "p":
@@ -523,11 +459,4 @@ func convertConstraintType(t string) string {
 	default:
 		return t
 	}
-}
-
-func convertColumnNullable(str string) bool {
-	if str == "NO" {
-		return false
-	}
-	return true
 }
