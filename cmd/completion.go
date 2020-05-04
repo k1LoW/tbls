b49acd02ac955b2a0ac2c097c3ec0150e507d82d
k1LoW <k1lowxb@gmail.com>
2020-05-04 22:02:37 +0900

    Support completion of external subcommands (Zsh)

--- cmd/completion.go
+++ cmd/completion.go
@@ -83,7 +83,7 @@ tbls completion zsh > $fpath[1]/_tbls
 				os.Exit(1)
 			}
 		case "zsh":
-			if err := rootCmd.GenZshCompletion(o); err != nil {
+			if err := genZshCompletion(rootCmd, o); err != nil {
 				_, _ = fmt.Fprintf(os.Stderr, "%s\n", err)
 				os.Exit(1)
 			}
